7cf528855767776ddc55fcab0ad719075f2aeec0
rasel.mahmud <rasel.mahmud@spectrum-bd.com>
2024-07-24 05:55:07 +0600

    Build directory explower

--- app/src/main/java/com/example/rs_app/screens/FileExplore.kt
+++ app/src/main/java/com/example/rs_app/screens/FileExplore.kt
@@ -1,14 +1,12 @@
 package com.example.rs_app.screens
 
-import android.annotation.SuppressLint
 import android.os.Environment
-import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -19,7 +17,6 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -33,7 +30,17 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import java.io.File
+
+data class FileFolder(
+    val name: String = "",
+    val size: Long = 0,
+    val absolutePath: String = "",
+    val isFile: Boolean = false,
+    val lastModified: Long = 0,
+)
 
 
 fun readDirectoryContents(directoryContents: MutableList<String>) {
@@ -46,84 +53,167 @@ fun readDirectoryContents(directoryContents: MutableList<String>) {
     }
 }
 
-@SuppressLint("InvalidColorHexValue")
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FileExploreScreen(n: NavHostController) {
     var hasPermission by remember { mutableStateOf(false) }
 
-    val directoryContents = remember { mutableStateListOf<String>() }
+
+    val directoryContents = remember { mutableStateListOf<FileFolder>() }
     val selectedForSync = remember { mutableStateListOf<String>() }
+    val prevPath = remember { mutableStateOf("/storage/emulated/0/") }
+
+    val aFile = "/storage/emulated/0/Android"
 
     val localContext = LocalContext.current
 
-    val permissionLauncher = rememberLauncherForActivityResult(
-        contract = ActivityResultContracts.RequestPermission()
-    ) { isGranted ->
-        hasPermission = isGranted
-        if (isGranted) {
-            readDirectoryContents(directoryContents)
-        } else {
-            Toast.makeText(localContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+//    val permissionLauncher = rememberLauncherForActivityResult(
+//        contract = ActivityResultContracts.RequestPermission()
+//    ) { isGranted ->
+//        hasPermission = isGranted
+//        if (isGranted) {
+//            readDirectoryContents(directoryContents)
+//        } else {
+//            Toast.makeText(localContext, "Permission Denied", Toast.LENGTH_SHORT).show()
+//        }
+//    }
+
+    fun mapToListed(files: Array<File>?) {
+        if (files != null) {
+            for (file in files) {
+                directoryContents.add(
+                    FileFolder(
+                        name = file.name,
+                        size = file.length(),
+                        isFile = file.isFile,
+                        absolutePath = file.absolutePath,
+                        lastModified = file.lastModified()
+                    )
+                )
+            }
         }
+    }
+
+    fun expandListOfFiles(absolutePath: String) {
+        val file = File(absolutePath)
+        directoryContents.clear()
+        mapToListed(file.listFiles())
+        prevPath.value = file.parent.toString()
     }
 
     LaunchedEffect(Unit) {
         val directory = Environment.getExternalStorageDirectory()
         val files = directory.listFiles()
-        if (files != null) {
-            for (file in files) {
-                directoryContents.add(file.name)
-            }
-        }
+        mapToListed(files)
+    }
+
+    fun handleDeleteFile() {
+//        Os.remove("")
     }
 
 
     Box(
         modifier = Modifier
             .fillMaxWidth()
+            .background(Color(0xFF1E2444))
 
     ) {
 
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(40.dp)
-        ) {
-            Button(onClick = {
-
-                if (selectedForSync.count() == directoryContents.count()) {
+        Column {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween
+            ) {
+                Button(onClick = {
+                    if (selectedForSync.count() == directoryContents.count()) {
 //                    selectedForSync.removeAll(selectedForSync)
-                    selectedForSync.clear()
-                } else {
-                    selectedForSync.addAll(directoryContents)
+                        selectedForSync.clear()
+                    } else {
+//                    selectedForSync.addAll(directoryContents)
+                    }
+                }) {
+                    Text(text = "Select All")
                 }
-            }) {
-                Text(text = "Select All")
+
+                Button(onClick = {
+                    handleDeleteFile()
+                }) {
+                    Text(text = "Delete")
+                }
+            }
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(35.dp)
+                    .padding(0.dp, 0.dp, 0.dp, 0.dp)
+                    .background(Color(0xBA5F6786))
+                    .clickable(enabled = true, onClick = {
+                        if (prevPath.value != "/storage/emulated") {
+                            directoryContents.clear()
+                            expandListOfFiles(prevPath.value)
+                        }
+                    }),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    "... back...",
+                    modifier = Modifier.padding(33.dp, 0.dp),
+                    fontSize = 14.sp,
+                    color = Color(
+                        0xFFE4E4E4
+                    )
+                )
+
             }
         }
 
         LazyColumn(
             contentPadding = PaddingValues(0.dp),
             verticalArrangement = Arrangement.spacedBy(0.dp),
-            modifier = Modifier.padding(0.dp, 40.dp, 0.dp, 0.dp)
+            modifier = Modifier.padding(0.dp, 90.dp, 0.dp, 0.dp)
         ) {
-
             items(directoryContents) { item ->
                 Row(
+
                     modifier = Modifier
-                        .background(Color(0x80EC759D))
                         .fillParentMaxWidth()
+                        .clickable(enabled = true, onClick = {
+                            expandListOfFiles(item.absolutePath)
+                        })
                 ) {
-                    val findIndex = selectedForSync.indexOf(item)
+                    val findIndex = selectedForSync.indexOf(item.name)
                     Checkbox(checked = findIndex != -1, onCheckedChange = {
                         if (findIndex == -1) {
-                            selectedForSync.add(item)
+                            selectedForSync.add(item.name)
                         } else {
                             selectedForSync.removeAt(findIndex)
                         }
                     })
-                    Text(item)
+                    Column(modifier = Modifier) {
+                        Row {
+                            Text(
+                                item.name, modifier = Modifier, fontSize = 14.sp, color = Color(
+                                    0xFFE4E4E4
+                                )
+                            )
+                        }
+                        Row {
+                            Text(
+                                (item.size / 1024).toString() + "Kb",
+                                modifier = Modifier,
+                                fontSize = 12.sp,
+                                color = Color(0xFFA3A3A3)
+                            )
+                            Text(
+                                "Last modified: " + item.lastModified.toString(),
+                                modifier = Modifier.padding(10.dp, 0.dp),
+                                fontSize = 12.sp,
+                                color = Color(0xFFA3A3A3)
+                            )
+                        }
+                    }
                 }
             }
         }
--- readme.md
+++ readme.md
@@ -30,3 +30,25 @@ emulator -avd Pixel_3a_XL_API_31
 emulator -avd Pixel_7_API_33
 emulator -avd Pixel_7_API_31
 ```
+
+
+
+
+
+./gradlew assembleRelease
+
+
+sudo apt-get install android-tools-adb
+
+cd TurboModuleDemo/DeviceName/android
+
+
+adb install ./android/app/build/outputs/apk/release/app-release.apk
+
+adb install -r ./android/app/build/outputs/apk/release/app-release.apk
+
+If you get an error saying the APK is already installed, you can add the -r flag to replace the existing installation:
+
+
+
+adb shell am start -n com.devicename/.MainActivity
